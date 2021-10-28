3a58563e5c77fb6dc6a67f73519ae7b164dacf65
Evandro-Cant <evandro.souza@direct.one>
2021-10-28 19:25:35 -0300

    feat: firebase initialize

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxPEyGI6yBVyvfCbmGgwo_mpTmNOPTls0",
+  authDomain: "nosurprise-53b33.firebaseapp.com",
+  projectId: "nosurprise-53b33",
+  storageBucket: "nosurprise-53b33.appspot.com",
+  messagingSenderId: "605753324323",
+  appId: "1:605753324323:web:5245f128b785a11b4a9296",
+  measurementId: "G-HEZFJ0N8S2",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
