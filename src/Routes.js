1d8951307f66f9ff58a98e4bfb05192db59085b7
Evandro-Cant <evandro.souza@direct.one>
2022-01-12 05:01:43 -0300

    merging

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import SignIn from "./pages/auth/SignIn";
 import SignUp from "./pages/auth/SignUp";
 import { Navbar } from "./layout/Navbar";
-import { AuthContext, AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import { Dashboard } from "./components/dashboard/Dashboard";
-import { useState } from "react";
-import { useContext } from "react";
+
 
 function App() {
-  const [user, setUser] = useState(null);
 
   return (
     <>
